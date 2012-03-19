21e0c9866bc7d607a947fb5bb8bb61c9c50b225d
Stephen Duvick <sduvick@gmail.com>
2012-03-19 09:35:33 -0500

    Adding the changes that made aosp 2.2 boot (for me at least)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,26 +1,120 @@
-USE_CAMERA_STUB := true
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
 
 # inherit from the proprietary version
--include vendor/samsung/epic4g/BoardConfigVendor.mk
+-include vendor/samsung/sidekick/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
-TARGET_BOOTLOADER_BOARD_NAME := epic4g
+#TARGET_NO_RECOVERY := true
+#TARGET_NO_KERNEL := true
 
-BOARD_KERNEL_CMDLINE := console=ttyFIQ0 no_console_suspend
-BOARD_KERNEL_BASE := 0x30000000
-BOARD_PAGE_SIZE := 0x00001000
+TARGET_BOARD := SGH-T839
+TARGET_BOARD_PLATFORM := s5pc110
+TARGET_BOARD_PLATFORM_GPU := POWERVR_SGX540_120
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+# ARMv7-A Cortex-A8 architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ANDROID_ARM_LINKER := true
+
+BOARD_USES_HGL := true
+
+#BOARD_USES_NEXUS_S_LIBS := TRUE
+
+# Camera defines
+#BOARD_USES_FROYO := true
+USE_CAMERA_STUB := true
+
+BOARD_USES_GENERIC_AUDIO := true
+
+BOARD_USES_LIBSECRIL_STUB := true
+
+# WiFi defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION := VER_0_6_X
+WIFI_DRIVER_FW_STA_PATH := "/system/etc/wifi/bcm4329_sta.bin"
+WIFI_DRIVER_FW_AP_PATH := "/system/etc/wifi/bcm4329_aps.bin"
+BOARD_WLAN_DEVICE := bcm4329
+WIFI_DRIVER_MODULE_PATH := "/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt dhd_watchdog_ms=10 dhd_poll=1"
+WIFI_DRIVER_MODULE_NAME := "dhd"
+WIFI_IFACE_DIR  := "/data/wifi"
+WIFI_SUPP_CONFIG_FILE :=  "/data/wifi/bcm_supp.conf"
+CONFIG_DRIVER_WEXT := true
+
+# USB tethering
+RNDIS_DEVICE := "/sys/devices/virtual/sec/switch/tethering"
+
+# Bluetooth defines
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_HAVE_BLUETOOTH := true
+BT_USE_BTL_IF := true
+BT_ALT_STACK := true
+BRCM_BTL_INCLUDE_A2DP := true
+BRCM_BT_USE_BTL_IF := true
+WITH_A2DP := true
+
+
+# Device related defines
+
+TARGET_PREBUILT_KERNEL := device/samsung/sidekick/kernel
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
+BOARD_KERNEL_BASE := 0x02e00000
+
+#BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_USES_FFORMAT := true
+BOARD_RECOVERY_IGNORE_BOOTABLES := true
+
+BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
+BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
+BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
+BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
+# The size of a block that can be marked bad.
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/samsung/epic4g/kernel
-
-BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/epic4g/recovery_ui.c
-#BOARD_HAS_NO_SELECT_BUTTON := true
-
+BOARD_BOOT_DEVICE := /dev/block/bml7
+BOARD_DATA_DEVICE := /dev/block/mmcblk0p1
+BOARD_DATA_FILESYSTEM := auto
+BOARD_DATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_HAS_DATADATA := true
+BOARD_DATADATA_DEVICE := /dev/block/stl10
+BOARD_DATADATA_FILESYSTEM := auto
+BOARD_DATADATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_SYSTEM_DEVICE := /dev/block/stl9
+BOARD_SYSTEM_FILESYSTEM := auto
+BOARD_SYSTEM_FILESYSTEM_OPTIONS := llw,check=no
+BOARD_CACHE_DEVICE := /dev/block/stl11
+BOARD_CACHE_FILESYSTEM := auto
+BOARD_CACHE_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0p1
+#BOARD_EMMC_DEVICE := /dev/block/mmcblk0p1
+#BOARD_USES_BMLUTILS := true
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/sidekick/vibrant_recovery_ui.c
+#BOARD_HAS_SMALL_RECOVERY := true
